c5aa265d8ee4a870fe223b55f851b0a1947854d7
VINEET-SIKARWAR <vineetsikarwar17092005@gmail.com>
2025-09-28 11:03:38 +0530

    zod validation added

--- complaint_backend/src/routes/auth.ts
+++ complaint_backend/src/routes/auth.ts
@@ -2,16 +2,35 @@ import { Router, Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import {z} from "zod";
 
 const router = Router();
 const prisma = new PrismaClient();
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
+// Zod schemas
+const registerSchema = z.object({
+  name: z.string().min(2, "Name must be at least 2 characters"),
+  email: z.email("Invalid email address"),
+  password: z.string().min(6, "Password must be at least 6 characters"),
+  role: z.enum(["citizen", "staff", "admin"]).optional(), // default will be citizen
+});
+
+const loginSchema = z.object({
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(6, "Password must be at least 6 characters"),
+});
+
 // POST /api/auth/register
 router.post("/register", async (req: Request, res: Response) => {
   try {
-    const { email, password, name, role } = req.body;
+
+     const parsed = registerSchema.safeParse(req.body);
+    if (!parsed.success) {
+      return res.status(400).json({ errors: parsed.error.flatten().fieldErrors });
+    }
+    const { email, password, name, role } = parsed.data;
 
     // check if user already exists
     const existingUser = await prisma.user.findUnique({ where: { email } });

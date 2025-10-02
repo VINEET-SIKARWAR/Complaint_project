3850172f89e75f4bc00c98269f8c33f4c8104232
VINEET-SIKARWAR <vineetsikarwar17092005@gmail.com>
2025-10-02 15:18:39 +0530

    interface for Complaint

---
+++ complaint_frontend/src/types/Complaint.ts
@@ -0,0 +1,13 @@
+// src/types/Complaint.ts
+export interface Complaint {
+  id: number;
+  title?: string;
+  description?: string;
+  status: "OPEN" | "IN_PROGRESS" | "RESOLVED";
+  category?: string;
+  area?: string;
+  photoUrl?: string;
+  createdAt?: string;
+  reporter?: { name: string; email: string };
+   assignedTo?: { name: string; email: string }; // show assigned staff if available
+}

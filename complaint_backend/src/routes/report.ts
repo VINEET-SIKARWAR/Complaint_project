6a49f2dad282e74c7263b87ef9f715a546ac5c35
VINEET-SIKARWAR <vineetsikarwar17092005@gmail.com>
2025-10-03 18:38:11 +0530

    update the csv route

--- complaint_backend/src/routes/report.ts
+++ complaint_backend/src/routes/report.ts
@@ -1,7 +1,7 @@
 import { Router, Response } from "express";
 import { PrismaClient } from "@prisma/client";
 import { Parser } from "json2csv";
-import { AuthRequest,authenticate } from "../middlewares/auth";
+import { AuthRequest, authenticate } from "../middlewares/auth";
 
 const router = Router();
 const prisma = new PrismaClient();
@@ -9,14 +9,30 @@ const prisma = new PrismaClient();
 // GET /api/reports/csv(comma seprated value)
 router.get("/csv", authenticate, async (req: AuthRequest, res: Response) => {
   try {
-    if (req.user!.role === "citizen") {
+    if (req.user!.role === "citizen" || req.user!.role === "staff") {
       return res.status(403).json({ error: "Access denied" });
     }
 
-    const complaints = await prisma.complaint.findMany({
-      include: { reporter: true },
-    });
+    let complaints: any[] = [];
 
+
+    if (req.user!.role === "admin") {
+      if (!req.user!.hostelId) {
+        return res.status(400).json({ error: "Hostel ID missing for admin" });
+      }
+
+      // Admins only see complaints from their hostel
+      complaints = await prisma.complaint.findMany({
+        where: { hostelId: req.user!.hostelId },
+
+         include: { reporter: true, hostel: true },
+      });
+    } else if (req.user!.role === "chief_admin") {
+      // Chief admin sees all
+      complaints = await prisma.complaint.findMany({
+        include: { reporter: true, hostel: true },
+      });
+    }
     // Convert to plain objects
     const data = complaints.map((c) => ({
       id: c.id,
@@ -26,6 +42,7 @@ router.get("/csv", authenticate, async (req: AuthRequest, res: Response) => {
       area: c.area,
       status: c.status,
       reporter: c.reporter.email,
+      hostel: (c as any).hostel?.name || "-",
       createdAt: c.createdAt,
       updatedAt: c.updatedAt,
     }));
